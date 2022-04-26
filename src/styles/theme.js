8f8476c14184b38ee068f6a2d0a5826c5d014a32
Helisson Sousa <helissoncg@hotmail.com>
2022-04-25 23:43:23 -0300

    Initial

---
+++ src/styles/theme.js
@@ -0,0 +1,14 @@
+export const theme = {
+  colors: {
+    mainBg: '#0A1128',
+    secondaryBg: '#dc143c',
+    lightBg: '#0A1128',
+    darkBg: '#0A1128',
+  },
+  font: {
+    family: {
+      default: "'Open Sans', sans-serif",
+      secundary: "'Montserrat', sans-serif",
+    },
+  },
+};
